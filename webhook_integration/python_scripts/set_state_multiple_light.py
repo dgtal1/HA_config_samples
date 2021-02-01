09684258052c9e6f172e171814fe54c017db93dd
dgtal1 <27864579+dgtal1@users.noreply.github.com>
2021-02-01 20:06:43 +0100

    Create set_state_multiple_light.py

---
+++ webhook_integration/python_scripts/set_state_multiple_light.py
@@ -0,0 +1,27 @@
+""" 
+SCRIPT INPUT - JSON MESSAGE:
+{
+    "entities": [
+         {
+            "sensor__xiaomi_clock_salon_temperature":  "24.1"
+        },
+         {
+            "switch__room":  "on"
+        }        
+    ]
+}
+"""
+
+entities = data.get('payload').get('entities')
+
+for entity in entities:   
+    for entity_id, state in entity.items():
+
+        split = entity_id.split('__')
+        domain = split[0]
+        name = split[1]
+        entity = f"{domain}.{name}"
+
+        state_obj = hass.states.get(entity)
+        logger.error(entity)
+        hass.states.set(entity, state, state_obj.attributes)
